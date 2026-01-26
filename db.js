decb51c9b55a0eafc8cfb5c8db130d609295aeb6
Mahesh Tambe <165809491+mahesh-tambe07@users.noreply.github.com>
2026-01-26 20:32:31 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql2');
 const dotenv = require('dotenv');
 
-// Load environment variables from .env file
-dotenv.config();
+
 
 // MySQL connection setup
 const pool = mysql.createPool({
